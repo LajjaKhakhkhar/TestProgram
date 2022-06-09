7cdd2a8ac3e783fe750a6f0c3ae9545df526b020
LajjaKhakhkhar <40431971+LajjaKhakhkhar@users.noreply.github.com>
2022-06-09 15:38:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int[] arr = { 1,3,5,7,9};
+int max = arr.Max();
+int min = arr.Min();
+int minimumSum = 0;
+int maximumSum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] != max) {
+        minimumSum = minimumSum + arr[i];
+    }
+    if (arr[i] != min)
+    {
+        maximumSum = maximumSum + arr[i];
+    }
+}
+Console.WriteLine(minimumSum);
+Console.WriteLine(maximumSum);
